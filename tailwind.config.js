8c37fe817a9559b34f4e750bd652718fef75d9be
murdockfamilyroadside <joshuaadavis@live.com>
2024-04-29 23:45:57 -0400

    stats and hero blocks

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,14 @@
 module.exports = {
   content: ["index.html", "app.js"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'serif': ['IBM Plex Serif', 'serif'] // Adding 'IBM Plex Serif' as the default serif family
+      }
+    },
   },
   plugins: [],
 }
+
 
 
